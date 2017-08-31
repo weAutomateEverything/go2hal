bef8a68707b26221e813de64fe6f34d91e78666e
Marc Arndt <marc@marcarndt.com>
2017-08-31 11:09:36 +0200

    adding external DB config

--- database/Mongo.go
+++ database/Mongo.go
@@ -3,13 +3,14 @@ package database
 import (
 	"gopkg.in/mgo.v2"
 	"log"
+	"github.com/zamedic/go2hal/config"
 )
 
 var database *mgo.Database
 
 func init() {
 	log.Println("Starting Database")
-	session, err := mgo.Dial("localhost")
+	session, err := mgo.Dial(config.MongoAddress())
 	database = session.DB("hal")
 	if err != nil {
 		log.Panic(err)
