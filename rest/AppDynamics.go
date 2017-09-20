4aba85b9eec7e27eb20aac7d69656eb8c804aa0e
Marc Arndt <marc@marcarndt.com>
2017-09-20 16:23:46 +0200

    adding appd and delivery endpoints

--- rest/AppDynamics.go
+++ rest/AppDynamics.go
@@ -1,1 +1,20 @@
 package rest
+
+import (
+	"net/http"
+	"github.com/zamedic/go2hal/database"
+	"io/ioutil"
+	"log"
+)
+
+func receiveAppDynamicsAlert(w http.ResponseWriter, r *http.Request) {
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	database.SaveAudit("APPDYNAMICS",string(body))
+
+}
