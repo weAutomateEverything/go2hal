b7658e3fcd0faba9d53402c4b31e7319e40f9005
Marc Arndt <marc@marcarndt.com>
2018-01-30 13:08:29 +0200

    adding test

---
+++ bin/test.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+for pkg in $(go list ./... | grep -v vendor); do
+    go test -coverprofile=$(echo $pkg | tr / -).cover $pkg
+done
+echo "mode: set" > c.out
+grep -h -v "^mode:" ./*.cover >> c.out
+rm -f *.cover
