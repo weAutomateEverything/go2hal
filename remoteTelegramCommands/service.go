e48c3177a2ed35e709a18d628afb526e8726545c
Marc Arndt <marc@marcarndt.com>
2018-02-08 11:40:26 +0200

    adding initial work for remote telegram commands with grpc

---
+++ remoteTelegramCommands/service.go
@@ -0,0 +1,39 @@
+package remoteTelegramCommands
+
+import (
+	"github.com/zamedic/go2hal/telegram"
+	"gopkg.in/telegram-bot-api.v4"
+	"strconv"
+)
+
+type service struct {
+	telegram telegram.Service
+}
+
+func (s *service) RegisterCommand(request *RemoteCommandRequest, response RemoteCommand_RegisterCommandServer) error {
+	s.telegram.RegisterCommand(newRemoteCommand(request.Name, request.Description, response))
+	return nil
+}
+
+type remoteCommand struct {
+	name, help string
+	remote     RemoteCommand_RegisterCommandServer
+}
+
+func newRemoteCommand(name, help string, remote RemoteCommand_RegisterCommandServer) telegram.Command {
+	return &remoteCommand{name: name, help: help, remote: remote}
+
+}
+
+func (s remoteCommand) CommandIdentifier() string {
+	return s.name
+}
+
+func (s remoteCommand) CommandDescription() string {
+	return s.help
+}
+
+func (s remoteCommand) Execute(update tgbotapi.Update) {
+	request := RemoteRequest{Message: update.Message.Text, From: strconv.FormatInt(int64(update.Message.From.ID), 10)}
+	s.remote.Send(&request)
+}
