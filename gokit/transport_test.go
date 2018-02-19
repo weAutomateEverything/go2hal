a1b00a4448bbacdfb7edab450a39020d9ac8e26d
Marc Arndt <marc@marcarndt.com>
2018-02-19 14:55:38 +0200

    when sending a remote command, only send the additional fields

--- gokit/transport_test.go
+++ gokit/transport_test.go
@@ -1,22 +1,22 @@
 package gokit
 
 import (
-	"testing"
-	"net/http"
 	"io/ioutil"
+	"net/http"
+	"testing"
 )
 
 func TestEncodeRequest(t *testing.T) {
 	r := http.Request{}
-	EncodeRequest(nil,&r,"testing 123")
+	EncodeRequest(nil, &r, "testing 123")
 
 	result, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		t.Errorf("Esception in Encode Request %v",err)
+		t.Errorf("Esception in Encode Request %v", err)
 	}
 
-	if string(result) != "testing 123"{
-		t.Errorf("Unexpected result %v, expected testing 123",string(result))
+	if string(result) != "testing 123" {
+		t.Errorf("Unexpected result %v, expected testing 123", string(result))
 	}
 
 }
