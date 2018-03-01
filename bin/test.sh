c241287aaa4bcc437d4db52bfdf95ece555afa43
Marc Arndt <marc@marcarndt.com>
2018-03-01 16:44:07 +0200

    adding message for a delivery review

--- bin/test.sh
+++ bin/test.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 for pkg in $(go list ./... | grep -v vendor); do
-    go test -coverprofile=$(echo $pkg | tr / -).cover $pkg
+     if ! go test -coverprofile=$(echo $pkg | tr / -).cover $pkg; then
+        exit 1
+     fi
 done
 echo "mode: set" > c.out
 grep -h -v "^mode:" ./*.cover >> c.out
