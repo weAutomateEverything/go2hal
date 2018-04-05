4eaa0fa48c5ccccbd4d957e4be5a742e77a0478b
Marc Arndt <marc@marcarndt.com>
2018-04-05 11:57:00 +0200

    moving sbsa service out, replacing with a default callout service

---
+++ firstCall/service.go
@@ -0,0 +1,21 @@
+package firstCall
+
+import (
+	"context"
+	"os"
+)
+
+type Service interface {
+	GetFirstCall(ctx context.Context) (name string, number string, err error)
+}
+
+type defaultFirstCallService struct {
+}
+
+func NewDefaultFirstcallService() Service {
+	return &defaultFirstCallService{}
+}
+
+func (*defaultFirstCallService) GetFirstCall(ctx context.Context) (name string, number string, err error) {
+	return "DEFAULT", os.Getenv("DEFAULT_CALLOUT_NUMBER"), nil
+}
